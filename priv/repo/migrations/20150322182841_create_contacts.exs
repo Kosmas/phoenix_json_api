70b4430899f5ec2fbecd63d3db532c2701b3da60
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-03-22 19:31:46 +0100

    add create_contacts migration

---
+++ priv/repo/migrations/20150322182841_create_contacts.exs
@@ -0,0 +1,12 @@
+defmodule PhoenixJsonApi.Repo.Migrations.CreateContacts do
+  use Ecto.Migration
+
+  def change do
+    create table(:contacts) do
+      add :name
+      add :phone
+
+      timestamps
+    end
+  end
+end
