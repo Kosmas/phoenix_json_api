7bf2abb496bdee45dc455ef5d5d6836604e0a505
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-03-23 13:43:12 +0100

    factor out common test code to test/test_helper.exs

--- test/controllers/contact_controller_test.exs
+++ test/controllers/contact_controller_test.exs
@@ -1,17 +1,5 @@
 defmodule PhoenixJsonApi.ContactControllerTest do
-  use ExUnit.Case, asyn: false
-  use Plug.Test
-  alias PhoenixJsonApi.Contact
-  alias PhoenixJsonApi.Repo
-  alias Ecto.Adapters.SQL
-
-  setup do
-    SQL.begin_test_transaction(Repo)
-
-    on_exit fn ->
-      SQL.rollback_test_transaction(Repo)
-    end
-  end
+  use PhoenixJsonApi.Case, async: false
 
   test "/index returns a list of cntacts" do
     contacts_as_json =
@@ -24,11 +12,5 @@ defmodule PhoenixJsonApi.ContactControllerTest do
 
     assert response.status == 200
     assert response.resp_body == contacts_as_json
-  end
-
-  defp send_request(conn) do
-    conn
-    |> put_private(:plug_skip_csrf_protection, true)
-    |> PhoenixJsonApi.Endpoint.call([])
   end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,29 @@
+defmodule PhoenixJsonApi.Case do
+  use ExUnit.CaseTemplate
+  alias Ecto.Adapters.SQL
+  alias PhoenixJsonApi.Repo
+
+  setup do
+    SQL.begin_test_transaction(Repo)
+
+    on_exit fn ->
+      SQL.rollback_test_transaction(Repo)
+    end
+  end
+
+  using do
+    quote do
+      alias PhoenixJsonApi.Repo
+      alias PhoenixJsonApi.Contact
+      use Plug.Test
+
+      def send_request(conn) do
+        conn
+        |> put_private(:plug_skip_csrf_protection, true)
+        |> PhoenixJsonApi.Endpoint.call([])
+      end
+    end
+  end
+end
+
 ExUnit.start
